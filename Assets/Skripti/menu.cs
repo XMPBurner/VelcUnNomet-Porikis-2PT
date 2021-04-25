1903e29f220b08346d8f950e2505c380c5cf3f30
unknown <emilsporikis2@gmail.com>
2021-04-25 17:23:44 +0300

    gatavs

---
+++ Assets/Skripti/menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    public GameObject poga;
+    public GameObject pogab;
+    public GameObject attels;
+    // Start is called before the first frame update
+    public void izkritosais(){
+    attels.SetActive(true);
+    if(poga == true){
+        attels.SetActive(false);
+        pogab.SetActive(false);
+        poga.SetActive(false);
+    }
+    }
+}
